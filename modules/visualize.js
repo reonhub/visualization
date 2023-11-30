19c06d34062fdfedc60f23ad7ea299fd09269a9b
reonhub <akiyama@susdesign.t.u-tokyo.ac.jp>
2023-11-30 12:00:26 +0900

    デザイン・レイアウトの調整

--- modules/visualize.js
+++ modules/visualize.js
@@ -7,8 +7,12 @@ exports.fetchParameterImageUrl = function (input){
   axis = input.axis;
   index = input.inputFile1.match(/\d*/);
   range = input.colorRange;
-  url = `./image/${param}_${axis}_${index}_${range}.png`
-  return "./image/C軸加速度1.png";
+  if(param && axis && index && range){
+    url = `./image/${param}_${axis}_${index}_${range}.png`
+    return "./image/C軸加速度1.png";
+  } else {
+    return "https://via.placeholder.com/570x292/?text=Select\ correct\ parameter";
+  }
 }
 
 
