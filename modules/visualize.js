be4c044a84e02ede732a780ee4598c4a413de572
reonhub <akiyama@susdesign.t.u-tokyo.ac.jp>
2023-12-04 21:25:36 +0900

    Not Foundの表示

--- modules/visualize.js
+++ modules/visualize.js
@@ -1,7 +1,9 @@
+const fs = require("fs");
+
 exports.fetchErrorImageUrl = function (DOMAIN, input){
   const idx = input.logFile.replace(/[^0-9]/g, '');
   const range = input.colorRange;
-  if(input.modelFile == "3D形状モデル" && idx && range){
+  if(input.modelFile == "3D形状モデル" && idx && range && fs.existsSync(`./public/image/加工誤差_${idx}_${range}.png`)){
     return `http://${DOMAIN}:3000/image/加工誤差_${idx}_${range}.png`;
   } else {
     return "https://via.placeholder.com/570x292/?text=Not%20Found";
@@ -13,7 +15,7 @@ exports.fetchParameterImageUrl = function (DOMAIN, input){
   const axis = input.axis;
   const idx = input.logFile.replace(/[^0-9]/g, '');
   const range = input.colorRange;
-  if(param && axis && idx && range){
+  if(param && axis && idx && range && fs.existsSync(`./public/image/${param}_${axis}_${idx}_${range}.png`)){
     return `http://${DOMAIN}:3000/image/${param}_${axis}_${idx}_${range}.png`
   } else {
     return "https://via.placeholder.com/570x292/?text=Not%20Found";
